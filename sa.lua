6182a7609bd0fcab0164ad84b0e077c7cadba784
kip1231 <81984493+kip1231@users.noreply.github.com>
2024-09-28 15:20:34 +0500

    Update sa.lua

--- sa.lua
+++ sa.lua
@@ -1,34 +1,16 @@
 local KeyGuardLibrary = {}
-
 local TokenStorage = {}
-
--- Функция для установки значений
 function KeyGuardLibrary.Set(data)
-    if data.publicToken and data.privateToken and data.trueData and data.falseData then
-        -- Сохраняем данные в хранилище
-        TokenStorage.publicToken = data.publicToken
-        TokenStorage.privateToken = data.privateToken
-        TokenStorage.trueData = data.trueData
-        TokenStorage.falseData = data.falseData
-
-        -- Сообщаем об успешной установке данных
-        print("Данные успешно сохранены:")
-        print("Публичный токен: " .. TokenStorage.publicToken)
-        print("Приватный токен: " .. TokenStorage.privateToken)
-        print("TrueData: " .. tostring(TokenStorage.trueData))
-        print("FalseData: " .. tostring(TokenStorage.falseData))
-    else
-        print("Ошибка: не все необходимые данные были переданы.")
-    end
+    TokenStorage.publicToken = data.publicToken
+    TokenStorage.privateToken = data.privateToken
+    TokenStorage.trueData = data.trueData
+    TokenStorage.falseData = data.falseData
 end
--- Функция для получения ссылки с токеном (например, для активации)
 function KeyGuardLibrary.getLink()
     return "https://t.me/KrutoySuslik"
 end
-
-print("Бипасная" .. tostring(TokenStorage.trueData))
 function KeyGuardLibrary.validateDefaultKey(inputKey)
-    return TokenStorage.trueData
+    return tostring(TokenStorage.trueData)
 end
 
 return KeyGuardLibrary
